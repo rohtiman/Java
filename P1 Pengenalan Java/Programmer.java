e25a1481b03b3d272d3a524325ac30c6dc064e20
rohtiman <31527199+rohtiman@users.noreply.github.com>
2017-09-26 09:02:22 +0700

    Add files via upload

---
+++ P1 Pengenalan Java/Programmer.java
@@ -0,0 +1,7 @@
+public class Programmer {
+	// Contoh penggunaan ganti baris dengan Println
+	public static void main(String[] args) {
+		System.out.println("I am a Programmer,");
+		System.out.println("I have no life");
+	}
+}
---
+++ P1 Pengenalan Java/Programmer1.java
@@ -0,0 +1,7 @@
+public class Programmer1 {
+	// Contoh penggunaan ganti baris dengan \n
+	// Contoh penggunaan tab dengan  \t
+	public static void main(String[] args) {
+		System.out.println("I am a Programmer,\n\tI have no life");
+	}
+}
